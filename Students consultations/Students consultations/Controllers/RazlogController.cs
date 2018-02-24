f6e5b8edd467d48c521ae5505c7453c81c343787
Milena Suknovic <milenasuknovic@hotmail.com>
2018-02-24 11:56:12 +0100

    Added RazlogController

---
+++ Students consultations/Students consultations/Controllers/RazlogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using StudentsConsultations.Service.Interfaces;
+
+namespace StudentsConsultations.Controllers
+{
+    public class RazlogController : Controller
+    {
+        private IRazlogService _iRazlogService;
+        private IMapper _mapper;
+
+        public RazlogController(IRazlogService iRazlogService, IMapper mapper)
+        {
+            _iRazlogService = iRazlogService;
+            _mapper = mapper;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
